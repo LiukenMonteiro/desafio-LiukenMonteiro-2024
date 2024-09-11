c773d0a10f10b75fa0a7e103b556fb6247313db6
LiukenMonteiro <liuken37@gmail.com>
2024-09-11 11:42:54 -0300

    animais adicionados

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -7,7 +7,15 @@ class RecintosZoo {
             { numero: 3, bioma: 'savana e rio', tamanho: 7, animais: [{ especie: 'GAZELA', quantidade: 1 }] },
             { numero: 4, bioma: 'rio', tamanho: 8, animais: [] },
             { numero: 5, bioma: 'savana', tamanho: 9, animais: [{ especie: 'LEAO', quantidade: 1 }] }
-        ]
+        ];
+        this.animais = {
+            LEAO: { tamanho: 3, biomas: ['savana'], carnivoro: true },
+            LEOPARDO: { tamanho: 2, biomas: ['savana'], carnivoro: true },
+            CROCODILO: { tamanho: 3, biomas: ['rio'], carnivoro: true },
+            MACACO: { tamanho: 3, biomas: ['savana', 'floresta'], carnivoro: false },
+            GAZELA: { tamanho: 3, biomas: ['savana'], carnivoro: false },
+            HIPOPOTAMO: { tamanho: 3, biomas: ['savana', 'rio'], carnivoro: false }
+        };
     }
 
 
