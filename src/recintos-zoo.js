bc42cb1488f43c004381ae4f1df56e58c91ffa80
LiukenMonteiro <liuken37@gmail.com>
2024-09-11 11:32:03 -0300

    first line

---
+++ src/recintos-zoo.js
@@ -0,0 +1,15 @@
+class RecintosZoo {
+
+    constructor() {
+        this.recintos = [
+            { numero: 1, bioma: 'savana', tamanho: 10, animais: [{especie: 'MACACO', quantidade: 3}] },
+        ]
+    }
+
+
+    analisaRecintos(animal, quantidade) {
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
